bcc82366a46e9a6b2c7a90a7a7e3f2115ef76f07
Surjit Reghunathan <surjit.reghunathan@beamcommunications.com>
2017-08-22 15:44:51 +1000

    Updated the tddenv.py post testing

---
+++ TestDrivenDevelopment/src/tddenv.py
@@ -0,0 +1,31 @@
+""" This is a test driver framework example"""
+
+import unittest
+import tempfile
+import shutil
+import os
+import glob
+
+class TestDD(unittest.TestCase):
+	
+	def setUp(self):
+		self.orgdir = os.getcwd()
+		self.dirname = tempfile.mkdtemp("tempySurjit");
+		print("oCreated Directory",self.dirname)
+		os.chdir(self.dirname)
+
+	def test_1(self):
+		'''Verify if creation of file is possible'''
+		
+		for filename in ["index.html","LICENSE","README"]:
+			f = open(filename,"wb+")
+			f.write("Somedata\n")
+			f.close()
+			self.assertTrue(f.closed)
+
+	def test_emptydir(self):
+		''' If current directory is empty'''
+		self.assertTrue(glob.glob("*"),[],"Directory is not empty")
+
+if __name__ == "__main__":
+	unittest.main()
